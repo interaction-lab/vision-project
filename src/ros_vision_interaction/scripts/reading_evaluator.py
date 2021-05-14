3f7a82629e6d84254219fd9ce3584598933e577b
Emily Zhou <zhouxemily@gmail.com>
2021-05-14 15:09:38 -0700

    Move audio analyzer methods to ROS node
    Signed-off-by: Emily Zhou <zhouxemily@gmail.com>

--- src/ros_vision_interaction/scripts/reading_evaluator.py
+++ src/ros_vision_interaction/scripts/reading_evaluator.py
@@ -1,21 +1,45 @@
 #!/usr/bin/env python3.8
+import glob
+import os
 import pymongo
 import rospy
+import sys
+import time
 
+from pydub import AudioSegment, silence
 from std_msgs.msg import Bool
-from vision_project_tools import AudioAnalyzer, init_db
+from vision_project_tools import init_db
 from vision_project_tools import EngineStateDb as StateDb
 from controllers.vision_project_delegator import INITIAL_STATE_DB
+
+sys.path.append('/root/catkin_ws/src/ffmpeg')
 
 
 class ReadingEvaluator:
 
     def __init__(
             self,
-            audio_trimmer,
-            statedb
+            statedb,
+            upload_directory,
+            file_prefix,
+            extension="wav",
+            silence_threshold=-20,
+            chunk_size=10,
+            seconds_to_check_for_audio_files=5
     ):
-        self._audio_trimmer = audio_trimmer
+        if not os.path.exists(copy_directory):
+            os.makedirs(copy_directory)
+        if not os.path.exists(upload_directory):
+            raise FileNotFoundError("The upload directory \'{}\' does not exist".format(upload_directory))
+
+        self._upload_directory = upload_directory
+        self._copy_directory = copy_directory
+        self._file_prefix = file_prefix
+        self._extension = extension
+        self._silence_threshold = silence_threshold
+        self._chunk_size = chunk_size
+        self._seconds_to_check_for_audio_files = seconds_to_check_for_audio_files
+
         is_record_evaluation_topic = rospy.get_param("controllers/is_record/evaluation")
         self._is_record_subscriber = rospy.Subscriber(
             is_record_evaluation_topic,
@@ -29,15 +53,49 @@ class ReadingEvaluator:
         rospy.loginfo(is_record.data)
         if not is_record.data:
             rospy.loginfo("Evaluator callback starting")
-            reading_time = self._audio_trimmer.get_length_of_trimmed_audio()
+            reading_time = self.get_length_of_trimmed_audio()
             reading_eval_index = self._state_database.get("reading eval index")
             try:
                 num_of_words = self._state_database.get("reading eval data")[reading_eval_index]["word count"]
             except IndexError or KeyError:
+                rospy.logerr(f"Reading task data not found for index:{reading_eval_index}")
                 num_of_words = 0
             reading_speed = num_of_words/reading_time
             rospy.loginfo("Reading speed: {}".format(reading_speed))
             self._state_database.set("current eval score", reading_speed)
+
+    def get_length_of_trimmed_audio(self):
+        audio_file = self.find_audio_file()
+        if audio_file is None:
+            rospy.loginfo("Evaluation audio file not found")
+            return 0
+        audio_segment = self.get_and_trim_audio_from_file(audio_file, self._silence_threshold, self._chunk_size)
+        return audio_segment.duration_seconds
+
+    def get_and_trim_audio_from_file(self, file_name, silence_threshold, chunk_size):
+        if not file_name.endswith(self._extension):
+            raise TypeError("Must be a {} file".format(self._extension))
+        rospy.loginfo(f"File name: {file_name}")
+        audio = AudioSegment.from_wav(file_name)
+        leading_silence_index = silence.detect_leading_silence(audio, silence_threshold, chunk_size)
+        audio.reverse()
+        ending_silence_index = silence.detect_leading_silence(audio, silence_threshold, chunk_size)
+        full_audio_length = len(audio)
+        trimmed_audio = audio[leading_silence_index:full_audio_length - ending_silence_index]
+        rospy.loginfo(f"Original audio length: {audio.duration_seconds}")
+        rospy.loginfo(f"Trimmed audio length: {trimmed_audio.duration_seconds}")
+        return trimmed_audio
+
+    def find_audio_file(self):
+        list_of_audio_files = []
+        for _ in range(2*self._seconds_to_check_for_audio_files):
+            list_of_audio_files = glob.glob(os.path.join(self._upload_directory, "*." + self._extension))
+            if len(list_of_audio_files) > 0:
+                break
+            time.sleep(0.2)
+        for file in list_of_audio_files:
+            if file.find(self._file_prefix) >= 0:
+                return file
 
 
 if __name__ == "__main__":
@@ -54,16 +112,14 @@ if __name__ == "__main__":
 
     upload_directory = rospy.get_param("vision-project/data/data_capture")
     copy_directory = rospy.get_param("vision-project/data/evaluation")
-    audio_trimmer = AudioAnalyzer(
+
+    reading_evaluator = ReadingEvaluator(
+        state_database,
         upload_directory=upload_directory,
-        copy_directory=copy_directory,
         file_prefix="evaluation",
-        new_file_name="evaluation",
         extension="wav",
         silence_threshold=-20,
         chunk_size=10
     )
-
-    reading_evaluator = ReadingEvaluator(audio_trimmer, state_database)
 
     rospy.spin()
--- src/ros_vision_interaction/src/vision-interaction/vision_project_tools/__init__.py
+++ src/ros_vision_interaction/src/vision-interaction/vision_project_tools/__init__.py
@@ -1,7 +1,6 @@
 import logging
 
 from vision_project_tools.engine_statedb import EngineStateDb
-from vision_project_tools.audio_analyzer import AudioAnalyzer
 from vision_project_tools.vision_engine import VisionInteractionEngine
 
 logging.basicConfig(level=logging.INFO)
