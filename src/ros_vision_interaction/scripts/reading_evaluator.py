c446014c5f158ff0c8da684a1180396ccfe6ce33
emilyxzhou <zhouxemily@gmail.com>
2021-05-22 22:10:37 -0700

    Improve silence detection
    Signed-off-by: emilyxzhou <zhouxemily@gmail.com>

--- src/ros_vision_interaction/scripts/reading_evaluator.py
+++ src/ros_vision_interaction/scripts/reading_evaluator.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3.8
+import datetime
 import glob
 import os
 import pymongo
 import rospy
+import speech_recognition
 import sys
 import time
+import webrtcvad
 
 from pydub import AudioSegment, silence
 from std_msgs.msg import Bool
@@ -23,7 +26,7 @@ class ReadingEvaluator:
             upload_directory,
             file_prefix,
             extension="wav",
-            silence_threshold=-20,
+            silence_threshold=-35,
             chunk_size=10,
             seconds_to_check_for_audio_files=5
     ):
@@ -41,12 +44,15 @@ class ReadingEvaluator:
         self._is_record_subscriber = rospy.Subscriber(
             is_record_evaluation_topic,
             Bool,
-            callback=self.is_done_recording_callback,
+            callback=self.reading_analyzer_callback,
             queue_size=1
         )
-        self._state_database = statedb
 
-    def is_done_recording_callback(self, is_record):
+        self._state_database = statedb
+        self._vad = webrtcvad.Vad()
+        self._vad.set_mode(1)
+
+    def reading_analyzer_callback(self, is_record):
         rospy.loginfo(is_record.data)
         if not is_record.data:
             rospy.loginfo("Evaluator callback starting")
@@ -70,7 +76,8 @@ class ReadingEvaluator:
         list_of_audio_files = []
         for _ in range(self._seconds_to_check_for_audio_files):
             list_of_audio_files = glob.glob(os.path.join(self._upload_directory, "*." + self._extension))
-            if len(list_of_audio_files) > 0:
+            if len(list_of_audio_files) > 0 and \
+                    any(self._file_prefix in string for string in list_of_audio_files):
                 break
             time.sleep(0.2)
         for file in list_of_audio_files:
@@ -87,16 +94,49 @@ class ReadingEvaluator:
 
         rospy.loginfo(f"File name: {audio_file}")
         audio_segment = AudioSegment.from_wav(audio_file)
-        full_audio_length = len(audio_segment)
-        rospy.loginfo(f"Original audio length: {audio_segment.duration_seconds}")
+        audio_segment = self._amplify_speech(audio_segment)
 
-        leading_silence_index = silence.detect_leading_silence(audio_segment, self._silence_threshold, self._chunk_size)
-        audio_segment.reverse()
-        ending_silence_index = silence.detect_leading_silence(audio_segment, self._silence_threshold, self._chunk_size)
+        silent_segments = silence.detect_silence(audio_segment, silence_thresh=self._silence_threshold)
+        leading_silence_index = silent_segments[0][1]
+        ending_silence_index = silent_segments[len(silent_segments)-1][0]
+        rospy.loginfo(f"Leading silence index: {leading_silence_index}, ending silence index: {ending_silence_index}")
 
-        trimmed_audio = audio_segment[leading_silence_index:full_audio_length - ending_silence_index]
-        rospy.loginfo(f"Trimmed audio length: {trimmed_audio.duration_seconds}")
+        trimmed_audio = audio_segment[leading_silence_index:ending_silence_index]
+        file_path = f"/root/audio_test/trimmed_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.wav"
+        trimmed_audio.export(
+            file_path,
+            format="wav"
+        )
+        self._analyze_loudness(trimmed_audio)
+        rospy.loginfo(
+            f"Original length: {audio_segment.duration_seconds}\nTrimmed length: {trimmed_audio.duration_seconds}"
+        )
         return audio_segment.duration_seconds
+    
+    def _amplify_speech(self, audio_segment, chunk_size=10):
+        start_index = 0
+        duration = len(audio_segment)
+        # while start_index < duration:
+        #     if audio_segment[start_index:start_index+chunk_size]:
+        #         audio_segment[start_index:start_index+chunk_size].apply_gain(20)
+        #         start_index += chunk_size
+        self._analyze_loudness(audio_segment)
+        audio_segment = audio_segment.apply_gain(+10)
+        file_path = f"/root/audio_test/test_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.wav"
+        audio_segment.export(
+            file_path,
+            format="wav"
+        )
+        return audio_segment
+
+    def _analyze_loudness(self, audio_segment, chunk_size=10):
+        loudness_array = []
+        start_index = 0
+        duration = len(audio_segment)
+        while start_index < duration:
+            loudness_array.append(audio_segment[start_index:start_index+chunk_size].dBFS)
+            start_index += chunk_size
+        rospy.loginfo(loudness_array)
 
 
 if __name__ == "__main__":
@@ -118,7 +158,7 @@ if __name__ == "__main__":
         upload_directory=upload_directory,
         file_prefix="evaluation",
         extension="wav",
-        silence_threshold=-20,
+        silence_threshold=-40,
         chunk_size=10
     )
 
